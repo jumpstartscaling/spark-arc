31f1983e7540a1c9dd691c6c8b64f405cb82668d
cawcenter <watcher@chrisamaya.work>
2026-03-24 17:08:46 -0400

    fix: switch to sirv-cli for production to bypass host block

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,4 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: process.env.PUBLIC_SITE_URL || undefined,
-  vite: {
-    server: {
-      allowedHosts: true
-    },
-    preview: {
-      allowedHosts: true
-    }
-  }
 });
